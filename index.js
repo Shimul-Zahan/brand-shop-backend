7711ca6dc72dd7d94fb6c9d4c3a2082f975cbb4b
AlgoWebBot <shimulzahan636@gmail.com>
2023-10-18 10:28:16 +0600

    delete route update for my cart

--- index.js
+++ index.js
@@ -67,6 +67,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: id };
+            const result = await cartItem.deleteOne(query);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
