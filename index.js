3a2d3939c094e683834af856a48e316164558cf4
AlgoWebBot <shimulzahan636@gmail.com>
2023-10-18 08:16:50 +0600

    products add to db

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // cors state for cross origin request service. its create relationship between backend and fontend 
-const cors = require('cors');
 app.use(cors());
+app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@shimulclaster1.85diumq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,8 +25,33 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        app.get('/', (req, res) => {
-            res.send('Hello Mongo')
+        const database = client.db("productsDB");
+        const products = database.collection("products");
+
+        app.get('/products', async (req, res) => {
+            const allProducts = await products.find().toArray();
+            res.send(allProducts)
+        })
+        app.get('/brand/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { brand_name : id.toLocaleLowerCase() };
+            const searchProducts = await products.find(query).toArray();
+            res.send(searchProducts)
+        })
+
+        app.get('/details/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const query = { _id: new ObjectId(id) }
+            const product = await products.findOne(query);
+            res.send(product);
+        })
+
+        app.post('/products', async (req, res) => {
+            const product = req.body
+            // console.log(product)
+            const result = await products.insertOne(product);
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
